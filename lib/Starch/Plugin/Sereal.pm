7e42d69d564905a961cdb9d82dd38b82ff74cf1e
Aran Deltac <bluefeet@gmail.com>
2015-07-13 15:12:21 -0700

    Lots of little cleanups and typos.

--- lib/Starch/Plugin/Sereal.pm
+++ lib/Starch/Plugin/Sereal.pm
@@ -1,23 +1,5 @@
 package Starch::Plugin::Sereal;
 
-use Moo;
-use strictures 2;
-use namespace::clean;
-
-with qw(
-    Starch::Plugin::Bundle
-);
-
-sub bundled_plugins {
-    return [qw(
-        ::Sereal::Manager
-        ::Sereal::State
-    )];
-}
-
-1;
-__END__
-
 =head1 NAME
 
 Starch::Plugin::Sereal - Use Sereal for cloning and diffing Starch data structures.
@@ -37,31 +19,102 @@ with ones that use L<Sereal> which can be leaps and bounds faster than Storable.
 In this author's testing C<is_data_diff> will be about 3x faster with Sereal and
 C<clone_data> will be about 1.5x faster with Sereal.
 
+=cut
+
+use Sereal::Encoder;
+use Sereal::Decoder;
+use Types::Standard -types;
+
+use Moo::Role;
+use strictures 2;
+use namespace::clean;
+
+with qw(
+    Starch::Plugin::ForManager
+);
+
 =head1 MANAGER ATTRIBUTES
 
 =head2 sereal_encoder
 
 An instance of L<Sereal::Encoder>.
 
+=cut
+
+has sereal_encoder => (
+    is  => 'lazy',
+    isa => InstanceOf[ 'Sereal::Encoder' ],
+);
+sub _build_sereal_encoder {
+    return Sereal::Encoder->new();
+}
+
 =head2 sereal_decoder
 
 An instance of L<Sereal::Decoder>.
+
+=cut
+
+has sereal_decoder => (
+    is  => 'lazy',
+    isa => InstanceOf[ 'Sereal::Decoder' ],
+);
+sub _build_sereal_decoder {
+    return Sereal::Decoder->new();
+}
 
 =head2 canonical_sereal_encoder
 
 An instance of L<Sereal::Encoder> with the C<canonical> option set.
 
-=head1 MODIFIED STATE METHODS
+=cut
+
+has canonical_sereal_encoder => (
+    is  => 'lazy',
+    isa => InstanceOf[ 'Sereal::Encoder' ],
+);
+sub _build_canonical_sereal_encoder {
+    return Sereal::Encoder->new({ canonical => 1 });
+}
+
+=head1 MODIFIED MANAGER METHODS
 
 =head2 clone_data
 
 Modified to use L</sereal_encoder> and L</sereal_decoder> to clone
 a data structure.
 
+=cut
+
+sub clone_data {
+    my ($self, $data) = @_;
+
+    return $self->sereal_decoder->decode(
+        $self->sereal_encoder->encode( $data ),
+    );
+}
+
 =head2 is_data_diff
 
 Modified to use L</canonical_sereal_encoder> to encode the two data
 structures.
+
+=cut
+
+sub is_data_diff {
+    my ($self, $old, $new) = @_;
+
+    my $encoder = $self->canonical_sereal_encoder();
+
+    $old = $encoder->encode( $old );
+    $new = $encoder->encode( $new );
+
+    return 0 if $new eq $old;
+    return 1;
+}
+
+1;
+__END__
 
 =head1 SUPPORT
 
--- t/basic.t
+++ t/basic.t
@@ -9,29 +9,4 @@ Test::Starch->new(
     plugins => ['::Sereal'],
 )->test();
 
-my $starch = Starch->new(
-    plugins => ['::Sereal'],
-    store => { class=>'::Memory' },
-);
-
-my $state = $starch->state();
-
-my $data = { foo=>32, bar=>[1,2,3] };
-
-is_deeply(
-    $state->clone_data( $data ),
-    $data,
-    'cloned data',
-);
-
-ok(
-    (!$state->is_data_diff($data, $data)),
-    'data is not diff',
-);
-
-ok(
-    $state->is_data_diff(12, $data),
-    'data is diff',
-);
-
 done_testing();
